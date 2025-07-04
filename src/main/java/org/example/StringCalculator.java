0edbf61f80481c79879adaf7f2896bf7ea805a72
rishi-patel26 <22ituos038@ddu.ac.in>
2025-07-05 00:06:02 +0530

    Pass-Make newlines test pass

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -7,10 +7,12 @@ public class StringCalculator {
             return 0;
         }
 
-        if (numbers.contains(",")) {
-            String[] apart = numbers.split(",");
-            int sum = 0;
-            for (String part : apart) {
+        if(numbers.contains(",") || numbers.contains("\n")){
+
+            String normalizedNumbers = numbers.replace("\n", ",");
+            String[] apart = normalizedNumbers.split(",");
+            int sum=0;
+            for(String part : apart){
                 sum += Integer.parseInt(part);
             }
             return sum;
